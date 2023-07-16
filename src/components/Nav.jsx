94547a732ea48691dd4b67f8c43d21ee52a7624b
EwelinaManiecka <ewelina.maniecka@op.pl>
2023-07-16 14:33:18 +0200

    add in nav component function map

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 import Logo from "../assets/images/logo.svg";
+import Search from "../assets/images/search.svg";
+import Store from "../assets/images/store.svg";
 
 function Nav() {
+  const links = [
+    "Store",
+    "Mac",
+    "iPad",
+    "iPhone",
+    "Watch",
+    "AirPods",
+    "Tv & Home",
+    "Entertainment",
+    "Accessories",
+    "Support",
+  ];
+  const listLinks = links.map((link) => (
+    <li>
+      <a className="link-styled">{link}</a>
+    </li>
+  ));
+
   return (
     <nav className="nav-wrapper">
       <div className="nav-content">
@@ -11,6 +31,13 @@ function Nav() {
           </li>
           <li>
             <a className="link-styled">Store</a>
+          </li>
+          {listLinks}
+          <li>
+            <img src={Search} alt="Search" />
+          </li>
+          <li>
+            <img src={Store} alt="Store" />
           </li>
         </ul>
       </div>
